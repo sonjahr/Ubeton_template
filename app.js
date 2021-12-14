086eb61242dfbcde754a036ced9f18209e44bef6
oussamabadr <oussamabadr@gmail.com>
2021-12-14 05:21:52 +0100

    draft use express js

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
-const http = require('http');
+var express = require('express')
+var app = express()
 
-const hostname = '127.0.0.1';
-const port =0;
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
-
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Ubeton');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
+app.listen(8080, function () {
+  console.log('Listening on port 8080...')
+})
